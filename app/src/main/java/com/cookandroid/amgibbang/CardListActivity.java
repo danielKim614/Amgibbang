be70266c8f1ddd7009b76a08cde28c96e3e1687a
Seungki614 <sztddan@gmail.com>
2021-11-05 15:48:57 +0900

    2021.11.05_15:40_Seungki_CardList Making

---
+++ app/src/main/java/com/cookandroid/amgibbang/CardListActivity.java
@@ -0,0 +1,36 @@
+package com.cookandroid.amgibbang;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class CardListActivity extends AppCompatActivity {
+
+    private ImageView buttonBack;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonBack = findViewById(R.id.cardlist_buttonBack);
+        toolbar = findViewById(R.id.toolbar);
+
+        //액션바 변경하기(들어갈 수 있는 타입 : Toolbar type
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
