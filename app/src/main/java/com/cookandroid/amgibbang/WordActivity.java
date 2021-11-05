075d06115d6862b9dfc6427d8dcba48cfbf623ad
Seo Dabin <tjekqls7@naver.com>
2021-11-05 16:07:10 +0900

    test

---
+++ app/src/main/java/com/cookandroid/amgibbang/WordActivity.java
@@ -0,0 +1,18 @@
+package com.cookandroid.amgibbang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class WordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word);
+
+    }
+}
