14e6da47cd30c8f351cd04a44dc00402e515be45
천지홍 <clara5091@naver.com>
2021-11-06 15:05:45 +0900

    jihong_1주차push

--- app/src/main/java/com/cookandroid/amgibbang/MainActivity.java
+++ app/src/main/java/com/cookandroid/amgibbang/MainActivity.java
@@ -1,14 +1,18 @@
 package com.cookandroid.amgibbang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+
+        //toolbar지정
+        Toolbar main_toolbar=findViewById(R.id.main_toolbar);
+        setSupportActionBar(main_toolbar);
     }
 }
