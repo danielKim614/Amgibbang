30655e15f0797f6635fa784dd00e8cfe77481878
Seo Dabin <tjekqls7@naver.com>
2021-11-09 03:20:01 +0900

    calendar 추가

---
+++ app/src/main/java/com/cookandroid/amgibbang/CalendarActivity.java
@@ -0,0 +1,33 @@
+package com.cookandroid.amgibbang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        Toolbar toolbar = findViewById(R.id.calendar_toolbar);
+        setSupportActionBar(toolbar);  // 액션바를 없앴으니까 그걸 툴바가 대신하게 하기
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);    // 툴바 왼쪽에 뒤로가기 버튼 추가
+        getSupportActionBar().setDisplayShowTitleEnabled(false);  // 타이틀 안 보이게 하기
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.button_back);  // 뒤로가기 버튼 아이콘 수정
+
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        CalendarView calendarView = findViewById(R.id.calendar_calendar_view);
+
+    }
+}
--- app/src/main/java/com/cookandroid/amgibbang/WordActivity.java
+++ app/src/main/java/com/cookandroid/amgibbang/WordActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 public class WordActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,18 @@ public class WordActivity extends AppCompatActivity {
 
     public void onButtonClick(View view) {
         switch(view.getId()) {
-            case R.id.word_button_edit: // edit 버튼 눌리면 할 일..
+            case R.id.word_button_edit: ;  // 단어 상세 내용 편집하기 (edit 버튼 저장 버튼으로 바꾸고 텍스트뷰 editable하게 변경)
+            case R.id.word_button_prev_word: ; // 이전 단어로 넘어가기
+            case R.id.word_button_next_word: ; // 다음 단어로 넘어가기
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home: // toolbar의 back 버튼이 눌렸을 때
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
