e18c1b990fc300107637b733e9f6171c9a6cebc8
danielKim614 <ksg980614@gmail.com>
2021-11-09 21:57:13 +0900

    Seungki_CardList ListView ver.4 (UI완성, 오류수정)

---
+++ app/src/main/java/com/cookandroid/amgibbang/SingleItem.java
@@ -0,0 +1,35 @@
+package com.cookandroid.amgibbang;
+
+public class SingleItem {
+    String word;
+    String meaning;
+
+    public SingleItem(String word, String meaning) {
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    @Override
+    public String toString() {
+        return "SingleItem{" +
+                "word='" + word + '\'' +
+                ", meaning='" + meaning + '\'' +
+                '}';
+    }
+}
